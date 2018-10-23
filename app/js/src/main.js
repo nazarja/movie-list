8b9cb62956858cc868ae77e6c051d1bb4117c885
Sean Murphy <mail.seanmurphy@gmail.com>
2018-10-23 17:52:16 +0100

    Added Nav Menu and JS Functionality

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,39 +1,68 @@
-/* // Variables
-const logo = document.querySelector('#logo');
-const movieIcon = document.querySelector('.movie-icon');
-const menuIcon = document.querySelector('.menu-icon');
-const navParent = document.querySelector('#nav-parent');
-const navParents = document.querySelectorAll('.nav-parent');
-const mainContent = document.querySelector('#main-content');
 
+/*
+==================================================================
+    NAVIGATION
+==================================================================
+*/
+const navParent = document.querySelectorAll('.nav-parent');
+const navChild = document.querySelectorAll('.nav-child');
+const navItem = document.querySelectorAll('.nav-item');
 
-logo.onmouseenter = () => { movieIcon.innerHTML = 'movie';};
-logo.onmouseleave = () => { movieIcon.innerHTML = 'movie_filter';};
+// Event listener to manage active state
+// Decides where to user will go and content to call 
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
 
-// Mobile Nav Actions
+        let activeClass;
+        if (item.classList.contains('nav-parent')) {
+            activeClass = navParent;
+        } else {
+            activeClass = navChild;
+        }
+        manageActiveClass(activeClass);
+        item.classList.add('active');
+    })
+})
+
+// Iterate over nav items and remove active class
+function manageActiveClass(activeClass) {
+    for (let i = 0; i < activeClass.length; i++) {
+        if (activeClass[i].classList.contains('active')) {
+            activeClass[i].classList.remove('active');
+        }
+    }
+}
+
+/*
+==============================
+    MOBILE NAV ACTIONS
+==============================
+*/
+
+// Event listener to toggle button / menu state
+const menuIcon = document.querySelector('#menu-btn');
+const primaryNav = document.querySelector('#primary-nav');
 menuIcon.onclick = () => { 
-    // f menu is closed else reverse state
     if (menuIcon.innerHTML == 'menu') {
         menuIcon.innerHTML = 'close';
+        primaryNav.style.left = '0';
     } 
     else {
         menuIcon.innerHTML = 'menu';
+        primaryNav.style.left = '-140px';
+
     }
 };
 
-navParents.forEach(item => {
-    item.addEventListener('click', () => {
-
-       // Remove .active class 
-       for (let i = 0; i < navParents.length; i++) {
-           if (navParents[i].classList.contains('active')) {
-               navParents[i].classList.remove('active');
-           }
-       }
-
-       // Add .active class
-        item.classList.add('active');
-        // change second menu items
-        console.log(item.dataset.nav);
-    })
-}) */
+// When window is resized, reset to default position
+// Fix for resize browser window bug
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800 && primaryNav.style.left == '-140px') {
+        menuIcon.innerHTML = 'menu';
+        primaryNav.style.left = '0';
+        
+    }
+    else if (window.innerWidth < 800) {
+            primaryNav.style.left = '-140px';
+    }
+});
